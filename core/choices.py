b9c61902341eaa45011625d49688853caf31c309
Wilian Luna <wilianaraujo407@gmail.com>
2025-11-26 09:10:57 -0500

    Projeto

---
+++ core/choices.py
@@ -0,0 +1,89 @@
+YES_OR_NOT = (
+  ('', 'Selecione'),
+  ('SIM', 'Sim'),
+  ('NAO', 'Não')
+)
+
+TIPO_SOLIC = (
+  ('', 'Selecione'),
+  ('AUTORIZAÇÃO', 'Autorização'),
+  ('RENOVACAO', 'Renovação')
+)
+
+SEXO = (
+  ('', 'Selecione'),
+  ('M', 'Masculino'),
+  ('F', 'Feminino')
+)
+
+ESTADOS_BRASIL_CHOICES = [
+  ('', 'Selecione'),
+  ('AC', 'Acre'),
+  ('AL', 'Alagoas'),
+  ('AP', 'Amapá'),
+  ('AM', 'Amazonas'),
+  ('BA', 'Bahia'),
+  ('CE', 'Ceará'),
+  ('DF', 'Distrito Federal'),
+  ('ES', 'Espírito Santo'),
+  ('GO', 'Goiás'),
+  ('MA', 'Maranhão'),
+  ('MT', 'Mato Grosso'),
+  ('MS', 'Mato Grosso do Sul'),
+  ('MG', 'Minas Gerais'),
+  ('PA', 'Pará'),
+  ('PB', 'Paraíba'),
+  ('PR', 'Paraná'),
+  ('PE', 'Pernambuco'),
+  ('PI', 'Piauí'),
+  ('RJ', 'Rio de Janeiro'),
+  ('RN', 'Rio Grande do Norte'),
+  ('RS', 'Rio Grande do Sul'),
+  ('RO', 'Rondônia'),
+  ('RR', 'Roraima'),
+  ('SC', 'Santa Catarina'),
+  ('SP', 'São Paulo'),
+  ('SE', 'Sergipe'),
+  ('TO', 'Tocantins'),
+]
+
+MUNICIPIOS_CHOICES = [
+  ('', 'Selecione'),
+  ('ACRELANDIA', 'Acrelândia'),
+  ('ASSIS_BRASIL', 'Assis Brasil'),
+  ('BRASILEIA', 'Brasiléia'),
+  ('BUJARI', 'Bujari'),
+  ('CAPIXABA', 'Capixaba'),
+  ('CRUZEIRO_SUL', 'Cruzeiro do Sul'),
+  ('EPITACIOLANDIA', 'Epitaciolândia'),
+  ('FEIJO', 'Feijó'),
+  ('JORDAO', 'Jordão'),
+  ('MANOEL_URB', 'Manoel Urbano'),
+  ('MARECHAL_THAU', 'Marechal Thaumaturgo'),
+  ('MANCIO_LIMA', 'Mâncio Lima'),
+  ('PLACIDO_CAS', 'Plácido de Castro'),
+  ('PORTO_ACRE', 'Porto Acre'),
+  ('PORTO_WALT', 'Porto Walter'),
+  ('RIO_BRANCO', 'Rio Branco'),
+  ('RODRIGUES_ALV', 'Rodrigues Alves'),
+  ('SANTA_ROSA', 'Santa Rosa do Purus'),
+  ('SENA_MADURE', 'Sena Madureira'),
+  ('SENADOR_GUIOM', 'Senador Guiomard'),
+  ('TARAUACA', 'Tarauacá'),
+  ('XAPURI', 'Xapuri'),
+]
+
+CHOICES_AREA_ATUACAO = [
+  ('', 'Selecione'),
+  ('FAUNA', 'Fauna'),
+  ('FLORA', 'Flora'),
+  ('ECOLOGIA', 'Ecologia'),
+  ('GEOLOGIA', 'Geologia'),
+  ('SOCIOECONOMIA', 'Socioeconomia'),
+  ('ARQUEOLOGIA', 'Arqueologia'),
+  ('TURISMO', 'Turismo'),
+  ('RECURSOS HIDRICOS', 'Recursos hidricos'),
+  ('EDUCAÇÃO AMBIENTAL', 'Educação Ambiental'),
+  ('CAVIDADES NATURAIS', 'Cavidades Naturais'),
+  ('OUTROS', 'Outros')
+]
